b7184f600ba22d6c9823cb75adb2bde8191b80dd
Angelo Ricardo <angeloengcomp@gmail.com>
2023-02-05 22:28:30 -0300

    chore: create user controller

--- src/app/controllers/user/CreateUserController.ts
+++ src/app/controllers/user/CreateUserController.ts
@@ -3,15 +3,54 @@ import { prismaClient } from "../../../database/prismaClient";
 
 export class CreateUserController {
   async handle(request: Request, response: Response) {
-    const { name, email } = request.body;
+    const { name, email, role, password, password_confirmation } = request.body;
 
-    const category = await prismaClient.user.create({
-      data: {
-        name,
+    const requiredData = [
+      "name",
+      "email",
+      "role",
+      "password",
+      "password_confirmation",
+    ];
+
+    for (const data of requiredData) {
+      if (!request.body[data]) {
+        return response.status(400).json({
+          error: `O campo ${data} é obrigatório`,
+        });
+      }
+    }
+
+    if (password !== password_confirmation) {
+      return response.status(400).json({
+        error: "Senhas não conferem",
+      });
+    }
+
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
         email,
       },
     });
 
-    return response.json(category);
+    if (userAlreadyExists) {
+      return response.status(400).json({
+        message: "Usuário já cadastrado",
+      });
+    }
+
+    const user = await prismaClient.user.create({
+      data: {
+        name,
+        email,
+        password,
+        role,
+      },
+    });
+
+    return response.json({
+      message: "Usuário criado com sucesso",
+      results: user,
+    });
   }
 }
