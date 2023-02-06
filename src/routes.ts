05ff564877a4031e65311245450bd642ebc469d8
Angelo Ricardo <angeloengcomp@gmail.com>
2023-02-05 22:28:44 -0300

    chore: create initial routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { CreateUserController } from "./app/controllers/user/CreateUserController";
-import { ShowUsersController } from "./app/controllers/user/ShowUsersControlle";
+import { CreateProjectController } from "./app/controllers/project/CreateProjectController";
+import { CreateProjectTimeController } from "./app/controllers/project-time/CreateProjectTimeController";
 
 const createUserController = new CreateUserController();
-const showUsersController = new ShowUsersController();
+const createProjectController = new CreateProjectController();
+const createProjectTimeController = new CreateProjectTimeController();
 
 const router = Router();
 
@@ -12,6 +14,7 @@ router.get("/api/v1", (req, res) => {
 });
 
 router.post("/api/v1/user", createUserController.handle);
-router.get("/api/v1/user", showUsersController.handle);
+router.post("/api/v1/project", createProjectController.handle);
+router.post("/api/v1/projectTime", createProjectTimeController.handle);
 
 export { router };
