37ae116661d786f0e572e1d6c3d3982953a5c4da
Angelo Ricardo <angeloengcomp@gmail.com>
2023-02-05 22:28:11 -0300

    chore: create project time controller

---
+++ src/app/controllers/project/CreateProjectController.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import { prismaClient } from "../../../database/prismaClient";
+
+export class CreateProjectController {
+  async handle(request: Request, response: Response) {
+    const { name, type } = request.body;
+
+    const requiredData = [
+      "name",
+      "type",
+    ];
+
+    for (const data of requiredData) {
+      if (!request.body[data]) {
+        return response.status(400).json({
+          error: `O campo ${data} é obrigatório`,
+        });
+      }
+    }
+
+    const projectAlreadyExists = await prismaClient.project.findFirst({
+      where: {
+        name,
+      },
+    });
+
+    if (projectAlreadyExists) {
+      return response.status(400).json({
+        message: "Projeto já cadastrado",
+      });
+    }
+
+    const project = await prismaClient.project.create({
+      data: {
+        name,
+        type
+      },
+    });
+
+    return response.json({
+      message: "Projeto criado com sucesso",
+      results: project,
+    });
+  }
+}
